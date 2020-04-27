8b3d4dd73f55fe576cdf90a948320d59ebc3e40e
Brad Nussbaum <brad@graphfoundation.org>
2020-04-27 17:32:40 -0400

    Reverting to 4.0 community

--- community/consistency-check/src/main/java/org/neo4j/consistency/checking/index/IndexAccessors.java
+++ community/consistency-check/src/main/java/org/neo4j/consistency/checking/index/IndexAccessors.java
@@ -2,12 +2,9 @@
  * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
  *
- * Copyright (c) 2018-2020 "Graph Foundation"
- * Graph Foundation, Inc. [https://graphfoundation.org]
+ * This file is part of Neo4j.
  *
- * This file is part of ONgDB.
- *
- * ONgDB is free software: you can redistribute it and/or modify
+ * Neo4j is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
