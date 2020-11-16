6917e632c1982a236325790c34ccab52fda34997
Brad Nussbaum <brad@graphfoundation.org>
2020-11-16 11:45:48 -0500

    Removing unused imports

--- community/kernel/src/main/java/org/neo4j/kernel/api/schema/MultiTokenSchemaDescriptor.java
+++ community/kernel/src/main/java/org/neo4j/kernel/api/schema/MultiTokenSchemaDescriptor.java
@@ -22,8 +22,6 @@ package org.neo4j.kernel.api.schema;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
 import java.util.Objects;
 
 import org.neo4j.internal.kernel.api.TokenNameLookup;
