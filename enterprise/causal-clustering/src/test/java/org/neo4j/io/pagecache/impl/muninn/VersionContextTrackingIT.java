59e67ba3ddd7bba9f9a5a49b62293379d8bb9890
Brad Nussbaum <brad@graphfoundation.org>
2021-02-08 21:01:05 -0500

    ONGDB-44 - VersionContextTrackingIT method does not override or implement a method from a supertype

--- enterprise/causal-clustering/src/test/java/org/neo4j/io/pagecache/impl/muninn/VersionContextTrackingIT.java
+++ enterprise/causal-clustering/src/test/java/org/neo4j/io/pagecache/impl/muninn/VersionContextTrackingIT.java
@@ -207,12 +207,6 @@ public class VersionContextTrackingIT
         }
 
         @Override
-        protected void releaseCursor()
-        {
-            delegate.releaseCursor();
-        }
-
-        @Override
         public boolean next() throws IOException
         {
             return delegate.next();
