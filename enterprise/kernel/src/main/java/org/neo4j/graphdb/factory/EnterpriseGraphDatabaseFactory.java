e46830282ec12a7bb3eaf717f0a022fe863b79a7
Brad Nussbaum <brad@graphfoundation.org>
2023-12-01 18:25:48 -0500

    Updates to new Config interface that accepts GraphDatabaseSettings and Settings constants

--- enterprise/kernel/src/main/java/org/neo4j/graphdb/factory/EnterpriseGraphDatabaseFactory.java
+++ enterprise/kernel/src/main/java/org/neo4j/graphdb/factory/EnterpriseGraphDatabaseFactory.java
@@ -41,7 +41,6 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.configuration.Config;
 import org.neo4j.kernel.enterprise.EnterpriseGraphDatabase;
 import org.neo4j.kernel.impl.factory.Edition;
-import org.neo4j.graphdb.facade.GraphDatabaseFacadeFactory;
 
 import static org.neo4j.kernel.configuration.Settings.FALSE;
 
@@ -67,10 +66,8 @@ public class EnterpriseGraphDatabaseFactory extends GraphDatabaseFactory
             @Override
             public GraphDatabaseService newDatabase( Config config )
             {
-                config.augment( GraphDatabaseFacadeFactory.Configuration.ephemeral, FALSE );
-                return new EnterpriseGraphDatabase( storeDir,
-                        config,
-                        state.databaseDependencies() );
+                config.augment( GraphDatabaseSettings.ephemeral, FALSE );
+                return new EnterpriseGraphDatabase( storeDir, config, state.databaseDependencies() );
             }
         };
     }
