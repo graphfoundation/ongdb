75d4df8427eb747ef88be58d090c7149170eb635
Brad Nussbaum <brad@graphfoundation.org>
2023-12-02 11:14:27 -0500

    JobScheduler.Groups.pageCacheIOHelper is now Group.PAGE_CACHE
    org.neo4j.scheduler.JobScheduler.JobHandle moved to org.neo4j.scheduler.JobHandle

--- enterprise/kernel/src/main/java/org/neo4j/kernel/impl/pagecache/WarmupAvailabilityListener.java
+++ enterprise/kernel/src/main/java/org/neo4j/kernel/impl/pagecache/WarmupAvailabilityListener.java
@@ -40,9 +40,9 @@ import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.kernel.availability.AvailabilityListener;
 import org.neo4j.kernel.configuration.Config;
 import org.neo4j.logging.Log;
+import org.neo4j.scheduler.Group;
+import org.neo4j.scheduler.JobHandle;
 import org.neo4j.scheduler.JobScheduler;
-
-import static org.neo4j.scheduler.JobScheduler.Groups.pageCacheIOHelper;
 
 class WarmupAvailabilityListener implements AvailabilityListener
 {
@@ -58,7 +58,7 @@ class WarmupAvailabilityListener implements AvailabilityListener
     // started yet, since we don't propagate an interrupt. This is why we check the `available` field in the
     // `scheduleProfile` method.
     private volatile boolean available;
-    private JobScheduler.JobHandle jobHandle; // Guarded by `this`.
+    private JobHandle jobHandle; // Guarded by `this`.
 
     WarmupAvailabilityListener( JobScheduler scheduler, PageCacheWarmer pageCacheWarmer,
                                 Config config, Log log, PageCacheWarmerMonitor monitor )
@@ -74,7 +74,7 @@ class WarmupAvailabilityListener implements AvailabilityListener
     public synchronized void available()
     {
         available = true;
-        jobHandle = scheduler.schedule( pageCacheIOHelper, this::startWarmup );
+        jobHandle = scheduler.schedule( Group.PAGE_CACHE, this::startWarmup );
     }
 
     private void startWarmup()
@@ -104,7 +104,7 @@ class WarmupAvailabilityListener implements AvailabilityListener
         }
         long frequencyMillis = config.get( GraphDatabaseSettings.pagecache_warmup_profiling_interval ).toMillis();
         jobHandle = scheduler.scheduleRecurring(
-                pageCacheIOHelper, this::doProfile, frequencyMillis, TimeUnit.MILLISECONDS );
+                Group.PAGE_CACHE, this::doProfile, frequencyMillis, TimeUnit.MILLISECONDS );
     }
 
     private void doProfile()
