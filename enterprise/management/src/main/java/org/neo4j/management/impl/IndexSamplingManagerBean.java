758454c13e19bc342383e5dfaf4de99bff078757
Brad Nussbaum <brad@graphfoundation.org>
2023-12-02 11:30:42 -0500

    org.neo4j.kernel.api.exceptions.index.IndexNotFoundKernelException moved to org.neo4j.internal.kernel.api.exceptions.schema.IndexNotFoundKernelException

--- enterprise/management/src/main/java/org/neo4j/management/impl/IndexSamplingManagerBean.java
+++ enterprise/management/src/main/java/org/neo4j/management/impl/IndexSamplingManagerBean.java
@@ -41,7 +41,7 @@ import org.neo4j.jmx.impl.ManagementBeanProvider;
 import org.neo4j.jmx.impl.ManagementData;
 import org.neo4j.jmx.impl.Neo4jMBean;
 import org.neo4j.kernel.NeoStoreDataSource;
-import org.neo4j.kernel.api.exceptions.index.IndexNotFoundKernelException;
+import org.neo4j.internal.kernel.api.exceptions.schema.IndexNotFoundKernelException;
 import org.neo4j.kernel.api.schema.SchemaDescriptorFactory;
 import org.neo4j.kernel.impl.api.index.IndexingService;
 import org.neo4j.kernel.impl.api.index.sampling.IndexSamplingMode;
