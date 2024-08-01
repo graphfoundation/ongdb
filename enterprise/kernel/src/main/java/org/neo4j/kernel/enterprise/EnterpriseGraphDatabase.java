11995e35fbef73c2fb04f68b9706bab3f95b2676
Brad Nussbaum <brad@graphfoundation.org>
2024-08-01 17:08:25 -0400

    Organizes imports for EnterpriseGraphDatabase

--- enterprise/kernel/src/main/java/org/neo4j/kernel/enterprise/EnterpriseGraphDatabase.java
+++ enterprise/kernel/src/main/java/org/neo4j/kernel/enterprise/EnterpriseGraphDatabase.java
@@ -36,11 +36,11 @@ package org.neo4j.kernel.enterprise;
 
 import java.io.File;
 
+import org.neo4j.graphdb.facade.GraphDatabaseFacadeFactory;
 import org.neo4j.kernel.configuration.Config;
 import org.neo4j.kernel.impl.enterprise.EnterpriseEditionModule;
 import org.neo4j.kernel.impl.factory.DatabaseInfo;
 import org.neo4j.kernel.impl.factory.GraphDatabaseFacade;
-import org.neo4j.graphdb.facade.GraphDatabaseFacadeFactory;
 
 public class EnterpriseGraphDatabase extends GraphDatabaseFacade
 {
