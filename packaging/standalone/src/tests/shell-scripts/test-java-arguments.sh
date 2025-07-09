b8eebc8932197d5eff7d89a903c86c38b4e92f23
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-07-09 17:11:31 -0400

    Simplify version handling
    ongdb does not need special memory information when running version

--- packaging/standalone/src/tests/shell-scripts/test-java-arguments.sh
+++ packaging/standalone/src/tests/shell-scripts/test-java-arguments.sh
@@ -5,22 +5,22 @@ test_description="Test Java arguments"
 . ./lib/sharness.sh
 fake_install
 
-test_expect_success "should set heap size constraints when checking version from wrapper conf" "
+test_expect_success "should not set heap size constraints when checking version from wrapper conf" "
   clear_config &&
   set_config 'dbms.memory.heap.initial_size' '512m' ongdb-wrapper.conf &&
   set_config 'dbms.memory.heap.max_size' '1024m' ongdb-wrapper.conf &&
   ongdb-home/bin/ongdb version || true &&
-  test_expect_java_arg '-Xms512m' &&
-  test_expect_java_arg '-Xmx1024m'
+  ! test_expect_java_arg '-Xms512m' &&
+  ! test_expect_java_arg '-Xmx1024m'
 "
 
-test_expect_success "should set heap size constraints when checking version" "
+test_expect_success "should not set heap size constraints when checking version" "
   clear_config &&
   set_config 'dbms.memory.heap.initial_size' '512m' ongdb.conf &&
   set_config 'dbms.memory.heap.max_size' '1024m' ongdb.conf &&
   ongdb-home/bin/ongdb version || true &&
-  test_expect_java_arg '-Xms512m' &&
-  test_expect_java_arg '-Xmx1024m'
+  ! test_expect_java_arg '-Xms512m' &&
+  ! test_expect_java_arg '-Xmx1024m'
 "
 
 for run_command in run_console run_daemon; do
