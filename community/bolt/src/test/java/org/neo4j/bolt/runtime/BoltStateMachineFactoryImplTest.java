2567b71af7b140879be19aa9ffa4c3e26441a517
Brad Nussbaum <brad@graphfoundation.org>
2026-06-27 22:49:41 -0400

    fix(bolt): restore Config import in factory test
    Test compile failed after BoltConnectionTracker wiring removed the
    Config import from BoltStateMachineFactoryImplTest.

--- community/bolt/src/test/java/org/neo4j/bolt/runtime/BoltStateMachineFactoryImplTest.java
+++ community/bolt/src/test/java/org/neo4j/bolt/runtime/BoltStateMachineFactoryImplTest.java
@@ -57,6 +57,7 @@ import org.neo4j.graphdb.DependencyResolver;
 import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.kernel.GraphDatabaseQueryService;
 import org.neo4j.kernel.api.bolt.BoltConnectionTracker;
+import org.neo4j.kernel.configuration.Config;
 import org.neo4j.kernel.impl.factory.GraphDatabaseFacade;
 import org.neo4j.logging.internal.NullLogService;
 import org.neo4j.test.OnDemandJobScheduler;
