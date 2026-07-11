ebc5d203583d857f881224c6159bdb5b04aa56b4
Brad Nussbaum <brad@graphfoundation.org>
2026-07-10 21:54:58 -0400

    Add BookmarkIT for causal consistency client contract.
    Prove concurrent commits still return distinct up-to-date bookmarks while
    one transaction is mid-apply, so drivers can rely on read-your-writes.

---
+++ integrationtests/src/test/java/org/neo4j/bolt/BookmarkIT.java
@@ -0,0 +1,250 @@
+/*
+ * Copyright (c) 2018-2020 "Graph Foundation,"
+ * Graph Foundation, Inc. [https://graphfoundation.org]
+ *
+ * This file is part of ONgDB Enterprise Edition. The included source
+ * code can be redistributed and/or modified under the terms of the
+ * GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+ * (http://www.fsf.org/licensing/licenses/agpl-3.0.html) as found
+ * in the associated LICENSE.txt file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.bolt;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.Transaction;
+import org.neo4j.graphdb.facade.GraphDatabaseFacadeFactory;
+import org.neo4j.graphdb.factory.GraphDatabaseFactoryState;
+import org.neo4j.graphdb.factory.module.PlatformModule;
+import org.neo4j.graphdb.factory.module.edition.AbstractEditionModule;
+import org.neo4j.graphdb.factory.module.edition.CommunityEditionModule;
+import org.neo4j.internal.kernel.api.exceptions.TransactionFailureException;
+import org.neo4j.io.IOUtils;
+import org.neo4j.kernel.configuration.Config;
+import org.neo4j.kernel.configuration.ConnectorPortRegister;
+import org.neo4j.kernel.impl.api.CommitProcessFactory;
+import org.neo4j.kernel.impl.api.TransactionCommitProcess;
+import org.neo4j.kernel.impl.api.TransactionRepresentationCommitProcess;
+import org.neo4j.kernel.impl.api.TransactionToApply;
+import org.neo4j.kernel.impl.factory.DatabaseInfo;
+import org.neo4j.kernel.impl.transaction.log.TransactionAppender;
+import org.neo4j.kernel.impl.transaction.tracing.CommitEvent;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.storageengine.api.StorageEngine;
+import org.neo4j.storageengine.api.TransactionApplicationMode;
+import org.neo4j.test.rule.TestDirectory;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.stream.Collectors.toSet;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.neo4j.kernel.configuration.Settings.TRUE;
+import static org.neo4j.test.assertion.Assert.assertEventually;
+
+/**
+ * Verifies the client-visible bookmark contract: while one transaction is mid-commit
+ * (appended but not yet applied), concurrent commits still return distinct, up-to-date
+ * bookmarks that drivers can use for causal consistency.
+ */
+public class BookmarkIT
+{
+    @Rule
+    public final TestDirectory directory = TestDirectory.testDirectory( getClass() );
+
+    private Driver driver;
+    private GraphDatabaseAPI db;
+
+    @After
+    public void tearDown() throws Exception
+    {
+        IOUtils.closeAllSilently( driver );
+        if ( db != null )
+        {
+            db.shutdown();
+        }
+    }
+
+    @Test
+    public void shouldReturnUpToDateBookmarkWhenSomeTransactionIsCommitting() throws Exception
+    {
+        CommitBlocker commitBlocker = new CommitBlocker();
+        db = createDb( commitBlocker );
+        driver = GraphDatabase.driver( boltAddress( db ) );
+
+        String firstBookmark = createNode( driver );
+
+        // make next transaction append to the log and then pause before applying to the store
+        // this makes it allocate a transaction ID but wait before acknowledging the commit operation
+        commitBlocker.blockNextTransaction();
+        CompletableFuture<String> secondBookmarkFuture = CompletableFuture.supplyAsync( () -> createNode( driver ) );
+        assertEventually( "Transaction did not block as expected", commitBlocker::hasBlockedTransaction, is( true ), 1, MINUTES );
+
+        Set<String> otherBookmarks = Stream.generate( () -> createNode( driver ) )
+                .limit( 10 )
+                .collect( toSet() );
+
+        commitBlocker.unblock();
+        String lastBookmark = secondBookmarkFuture.get();
+
+        // first and last bookmarks should not be null and should be different
+        assertNotNull( firstBookmark );
+        assertNotNull( lastBookmark );
+        assertNotEquals( firstBookmark, lastBookmark );
+
+        // all bookmarks received while a transaction was blocked committing should be unique
+        assertThat( otherBookmarks, hasSize( 10 ) );
+    }
+
+    private GraphDatabaseAPI createDb( CommitBlocker commitBlocker )
+    {
+        return createDb( platformModule -> new CustomCommunityEditionModule( platformModule, commitBlocker ) );
+    }
+
+    private GraphDatabaseAPI createDb( Function<PlatformModule,AbstractEditionModule> editionModuleFactory )
+    {
+        GraphDatabaseFactoryState state = new GraphDatabaseFactoryState();
+        GraphDatabaseFacadeFactory facadeFactory = new GraphDatabaseFacadeFactory( DatabaseInfo.COMMUNITY, editionModuleFactory );
+        return facadeFactory.newFacade( directory.databaseDir(), configWithBoltEnabled(), state.databaseDependencies() );
+    }
+
+    private static String createNode( Driver driver )
+    {
+        try ( Session session = driver.session() )
+        {
+            try ( Transaction tx = session.beginTransaction() )
+            {
+                tx.run( "CREATE ()" );
+                tx.success();
+            }
+            return session.lastBookmark();
+        }
+    }
+
+    private static Config configWithBoltEnabled()
+    {
+        Config config = Config.defaults();
+
+        config.augment( "dbms.connector.bolt.enabled", TRUE );
+        config.augment( "dbms.connector.bolt.listen_address", "localhost:0" );
+
+        return config;
+    }
+
+    private static String boltAddress( GraphDatabaseAPI db )
+    {
+        ConnectorPortRegister portRegister = db.getDependencyResolver().resolveDependency( ConnectorPortRegister.class );
+        return "bolt://" + portRegister.getLocalAddress( "bolt" );
+    }
+
+    private static class CustomCommunityEditionModule extends CommunityEditionModule
+    {
+        CustomCommunityEditionModule( PlatformModule platformModule, CommitBlocker commitBlocker )
+        {
+            super( platformModule );
+            commitProcessFactory = new CustomCommitProcessFactory( commitBlocker );
+        }
+    }
+
+    private static class CustomCommitProcessFactory implements CommitProcessFactory
+    {
+        final CommitBlocker commitBlocker;
+
+        private CustomCommitProcessFactory( CommitBlocker commitBlocker )
+        {
+            this.commitBlocker = commitBlocker;
+        }
+
+        @Override
+        public TransactionCommitProcess create( TransactionAppender appender, StorageEngine storageEngine, Config config )
+        {
+            return new CustomCommitProcess( appender, storageEngine, commitBlocker );
+        }
+    }
+
+    private static class CustomCommitProcess extends TransactionRepresentationCommitProcess
+    {
+        final CommitBlocker commitBlocker;
+
+        CustomCommitProcess( TransactionAppender appender, StorageEngine storageEngine, CommitBlocker commitBlocker )
+        {
+            super( appender, storageEngine );
+            this.commitBlocker = commitBlocker;
+        }
+
+        @Override
+        protected void applyToStore( TransactionToApply batch, CommitEvent commitEvent, TransactionApplicationMode mode ) throws TransactionFailureException
+        {
+            commitBlocker.blockWhileWritingToStoreIfNeeded();
+            super.applyToStore( batch, commitEvent, mode );
+        }
+    }
+
+    private static class CommitBlocker
+    {
+        final ReentrantLock lock = new ReentrantLock();
+        volatile boolean shouldBlock;
+
+        void blockNextTransaction()
+        {
+            shouldBlock = true;
+            lock.lock();
+        }
+
+        void blockWhileWritingToStoreIfNeeded()
+        {
+            if ( shouldBlock )
+            {
+                shouldBlock = false;
+                lock.lock();
+            }
+        }
+
+        void unblock()
+        {
+            lock.unlock();
+        }
+
+        boolean hasBlockedTransaction()
+        {
+            return lock.getQueueLength() == 1;
+        }
+    }
+}
